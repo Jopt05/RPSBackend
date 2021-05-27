df495584edfee1fc9ccded9ecb42efbe7161d3bc
= <=>
2021-05-27 16:07:19 -0500

    Fixed text in password length

--- routes/usuario.js
+++ routes/usuario.js
@@ -10,7 +10,7 @@ router.get('/', usuarioGet)
 router.post('/', 
 [
     check('user', 'El usuario no puede estar vacío').not().isEmpty(),
-    check('password', 'La password no puede quedar vacía').isLength({ min: 6 }),
+    check('password', 'La password debe ser mayor de 6 caracteres').isLength({ min: 6 }),
     check('score', 'La contraseña no puede estar vacía').not().isEmpty(),
     validarCampos
 ]
